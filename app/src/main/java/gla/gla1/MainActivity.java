3c21ffaa85cb0bbceb2beb37a3e49ef16147c982
anubhav2510 <anubhav.bansal_mca18@gla.ac.in>
2019-02-21 12:09:38 +0530

    update java code

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "shubham", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "vijay pal", Snackbar.LENGTH_LONG)
                         .setAction("ok", null).show();
             }
         });
