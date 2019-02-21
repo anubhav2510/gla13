afb5d5db3aa4cb0c8b0a0a5b1cf58c22b920c280
anubhav2510 <anubhav.bansal_mca18@gla.ac.in>
2019-02-21 12:56:16 +0530

    change name of snackbar

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Shubham Verma", Snackbar.LENGTH_LONG)
                         .setAction("ok", null).show();
             }
         });
